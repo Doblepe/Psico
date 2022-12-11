827e96f840ac025d23f001232962db5ad9ac9eb6
Doblepe <vicdobleperez@gmail.com>
2022-12-11 10:44:26 +0100

    classes

--- Classes/Niño.py
+++ Classes/Niño.py
@@ -1,5 +1,17 @@
+import Progenitor
 class Niño:
-    def __init__(self, id=int, nombre=str, horas=int ) -> None:
+    def __init__(self, 
+    id=int, 
+    nombre=str,
+    horas=int,
+    privado=bool,
+    progenitor=Progenitor, 
+    progenitor2= Progenitor,
+    informes = [{}] ) -> None:
         self.id = id
         self.nombre = nombre
         self.horas = horas
+        self.privado = privado
+        self.progenitor = Progenitor
+        self.progenitor2 = Progenitor
+        self.informes = None
---
+++ Classes/Progenitor.py
@@ -0,0 +1,4 @@
+class Progenitor:
+    def __init__(self, nombre = str, tfn=str) -> None:
+        self.nombre = nombre
+        self.tfn = tfn
