5f04fe564aab38b6489983c33a79d69278afe134
Doblepe <vicdobleperez@gmail.com>
2022-12-20 17:05:14 +0100

    Creating calendar tk

---
+++ Tk_widgets/Calendar.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkcalendar import Calendar
+from datetime import datetime
+
+fecha_actual = datetime.today()
+fecha_actual = datetime.strftime(fecha_actual, "%d/%m/%Y").split("/")
+
+actual_day = int(fecha_actual[0])
+actual_month = int(fecha_actual[1])
+actual_year = int(fecha_actual[2])
+
+
+# Create Object
+root = Tk()
+# Set geometry
+root.geometry("400x400")
+# Add Calendar
+cal = Calendar(root, selectmode='day',
+               year=actual_year, month=actual_month,
+               day=actual_day)
+
+cal.pack(pady=20)
+
+
+def grad_date():
+    date.config(text="Selected Date is: " + cal.get_date())
+
+
+# Add Button and Label
+Button(root, text="Get Date",
+       command=grad_date).pack(pady=20)
+
+date = Label(root, text="")
+date.pack(pady=20)
+
+# Execute Tkinter
+root.mainloop()
--- main.py
+++ main.py
@@ -3,55 +3,48 @@ import tkinter as tk
 from API.API import *
 from API.BBDD import *
 from datetime import datetime
-class Prueba:
-    def __init__(self, nombre, horas, privado, cita) -> None:
-       self.nombre = nombre
-       self.horas = horas
-       self.privado = privado
-       self.cita = cita
+from tkinter import *
+
+# class Prueba:
+#     def __init__(self, nombre, horas, privado, cita) -> None:
+#        self.nombre = nombre
+#        self.horas = horas
+#        self.privado = privado
+#        self.cita = cita
 
 
-fecha_actual = datetime.now()
-fecha_actual = datetime.strftime(fecha_actual, '%H:%M:%S --> %d/%B/%Y')
+# fecha_actual = datetime.now()
+# fecha_actual = datetime.strftime(fecha_actual, '%H:%M:%S --> %d/%B/%Y')
 
-progenitor1 = Progenitor_class('papá', '6623923i408'),
-progenitor2 = Progenitor_class('Mamá', '798719123'),
+# progenitor1 = Progenitor_class('papá', '6623923i408'),
+# progenitor2 = Progenitor_class('Mamá', '798719123'),
 
-prueba1 = Prueba('Juan', 3, 1, fecha_actual)
+# prueba1 = Prueba('Juan', 3, 1, fecha_actual)
 
-progenitor1 = str(progenitor1)
+# progenitor1 = str(progenitor1)
 
-# peque1 = Peque_class(1, 'Juan Mari', 3, 0, progenitor1, progenitor2)
-con = None
-try:
-    #BBDD.peques.insert_one({"nombre": "Marcelo", "Privado": True, "Número de horas": 10, "Padre": progenitor1})
-    BBDD.agenda.insert_one({"fecha Actual" :fecha_actual}) 
+# # peque1 = Peque_class(1, 'Juan Mari', 3, 0, progenitor1, progenitor2)
+# con = None
+# try:
+#     #BBDD.peques.insert_one({"nombre": "Marcelo", "Privado": True, "Número de horas": 10, "Padre": progenitor1})
+#     BBDD.agenda.insert_one({"fecha Actual" :fecha_actual}) 
 
-# {"Nombre padre": "Felipe", "Teléfono": "62910231089"}, {"Nombre Madre": "Felipona", "Teléfono": "1231289"}
-    print('Añadido')
-except Exception as e:
-    print(e)
+# # {"Nombre padre": "Felipe", "Teléfono": "62910231089"}, {"Nombre Madre": "Felipona", "Teléfono": "1231289"}
+#     print('Añadido')
+# except Exception as e:
+#     print(e)
 
-print('----------')
-for x in BBDD.peques.find().limit(5):
-  print(x)
-for x in BBDD.agenda.find().limit(5):
-  print(x)
+# print('----------')
+# for x in BBDD.peques.find().limit(5):
+#   print(x)
+# for x in BBDD.agenda.find().limit(5):
+#   print(x)
 
-# print(peque1)
-
-
-
-
-
+# # print(peque1)
 
 
 
-
-
-
-
-########### ----------- > Tkinter CODE
+# ########### ----------- > Tkinter CODE
 
 # root = tk.Tk()
 # root.title('Agenda')
@@ -60,11 +53,10 @@ for x in BBDD.agenda.find().limit(5):
 # root.geometry("1200x700")
 # click = 0
 # def funcion_click():
-#     global click
-#     click += 1
-#     print('Pulsado', click, "veces")
+#   for x in BBDD.agenda.find().limit(5):
+#     print(x)
 
-# button = tk.Button(root, fg= 'White', bg = 'blue', text = 'Haga click', width = 20, 
+# button = tk.Button(root, fg= 'White', bg = 'blue', text = 'show', width = 20, 
 # height = 10, command = funcion_click )
 # button.pack()
 # root.mainloop()
